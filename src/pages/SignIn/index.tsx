f300b7b49ca7859765a603554654802d6afd3ab7
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-26 18:30:29 -0300

    feature: Pages / Routes / Components - Criado Componentes de Botão e Input - Criado páginas de SignIn e SignUp - Adicionado roteamento

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('Button')}>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
